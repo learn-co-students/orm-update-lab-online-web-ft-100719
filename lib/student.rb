113331d64b58c9ddb4d6a1fc7988dcf117d537ef
github username <github email address>
2019-11-09 23:41:47 -0500

    too much at this point in the evening

--- lib/student.rb
+++ lib/student.rb
@@ -28,19 +28,43 @@ class Student
     DB[:conn].execute(sql)
   end
 
-    def save
-      if self.id
-        self.update
-      else
-        sql = <<-SQL
-          INSERT INTO students(name, grade)
-          VALUES(?, ?)
-        SQL
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
-        DB[:conn].execute(sql, self.name, self.grade)
-        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-      end
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students(name, grade)
+        VALUES(?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+  end
 
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
+  def self.new_from_db(row, id, name, grade)
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
+  end
 
 end
