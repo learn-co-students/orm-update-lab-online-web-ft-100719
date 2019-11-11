7fec5a5b6476d0c20ede248fdce993769f72fe2f
github username <github email address>
2019-11-11 09:21:17 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,10 +5,10 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade, :id
 
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
@@ -53,18 +53,25 @@ class Student
     student
   end
 
-  def self.new_from_db(row, id, name, grade)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name =  row[1]
-    new_student.grade = row[2]
-    new_student
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+
+    self.new(id, name, grade)
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
+   sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+  SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
 end
